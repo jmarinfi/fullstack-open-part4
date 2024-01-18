3ed7853378c3d8e4275d9d59008ec29d4652fbb3
Joan <jmarinfi@uoc.edu>
2024-01-17 21:01:39 -0500

    Exercise 4.9 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -18,6 +18,12 @@ test('there are two blogs', async () => {
     expect(response.body).toHaveLength(2)
 })
 
+test('the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+})
+
 
 afterAll(async () => {
     await mongoose.connection.close()
