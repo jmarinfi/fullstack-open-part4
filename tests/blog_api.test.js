57b9b664812a4b424f47328df8e1c2b575eb00c0
Joan <jmarinfi@uoc.edu>
2024-01-17 21:12:08 -0500

    Exercise 4.10 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+
+const initialBlogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
 
 
 test('blogs are returned as json', async () => {
@@ -22,6 +51,27 @@ test('the unique identifier property of the blog posts is named id', async () =>
     const response = await api.get('/api/blogs')
 
     expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "test blog",
+        author: "test author",
+        url: "test url",
+        likes: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(blog => blog.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toContain('test blog')
 })
 
 
